c27e8f4ebe524af575aa074a1c4014c6220c0e86
Alessandro Raffa <contact@alessandroraffa.eu>
2017-11-06 22:55:19 +0000

    Apply fixes from StyleCI

--- tests/JsonCacheTest.php
+++ tests/JsonCacheTest.php
@@ -3,18 +3,17 @@
 namespace Infobiotech;
 
 /**
- * JsonCacheTest Class
+ * JsonCacheTest Class.
  *
  * @author Alessandro Raffa, Infobiotech S.r.l. <a.raffa@infobiotech.net>
  */
 class JsonCacheTest extends \PHPUnit\Framework\TestCase
 {
-
-  public function testSet()
-  {
-    $cache = new JsonCache(
+    public function testSet()
+    {
+        $cache = new JsonCache(
         new \League\Flysystem\Adapter\Local(dirname(__FILE__)), 'myNamespace'
     );
-    $this->assertTrue($cache->set('key', 'value'));
-  }
+        $this->assertTrue($cache->set('key', 'value'));
+    }
 }
