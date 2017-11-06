f664d29b8a3d0e875bb42225edc1322794725938
Alessandro Raffa <contact@alessandroraffa.eu>
2017-11-06 03:12:21 +0100

    Formatting

--- tests/JsonCacheTest.php
+++ tests/JsonCacheTest.php
@@ -1,14 +1,19 @@
 <?php
+
 namespace Infobiotech;
+
 /**
  * JsonCacheTest Class
  *
  * @author Alessandro Raffa, Infobiotech S.r.l. <a.raffa@infobiotech.net>
  */
-class JsonCacheTest extends \PHPUnit_Framework_TestCase {
-  public function testSet() {
+class JsonCacheTest extends \PHPUnit_Framework_TestCase
+{
+
+  public function testSet()
+  {
     $cache = new JsonCache(
-      new \League\Flysystem\Adapter\Local(dirname(__FILE__))
+        new \League\Flysystem\Adapter\Local(dirname(__FILE__)), 'myNamespace'
     );
     $this->assertTrue($cache->set('key', 'value'));
   }
