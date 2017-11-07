f0f7cc217454cad66384eb9eded827b759c27f39
Alessandro Raffa <contact@alessandroraffa.eu>
2017-11-07 14:21:29 +0100

    Better flow and output

--- examples/cliExample.php
+++ examples/cliExample.php
@@ -3,60 +3,185 @@
  * For this example `composer require league/climate`
  */
 require 'vendor/autoload.php';
-$climate = new League\CLImate\CLImate();
-$climate->clear();
-$climate->out('');
-$climate->bold()->out('infobiotech/php-json-cache client example');
-$climate->out('');
-
-function fakeProcessTime()
-{
-    usleep(mt_rand(150000, 800000));
-}
+$climate           = new League\CLImate\CLImate();
+$climate->backgroundBlack()->clear();
+$climate->backgroundBlack()->white()->out('');
 /*
  *
  */
-$climate->out('');
-$climate->out('Creating JsonCache instance...');
-$jsonCache  = new Infobiotech\JsonCache(
-        new League\Flysystem\Adapter\Local('cache'), uniqid()
-);
-fakeProcessTime();
+$climate->addArt('art');
+$climate->backgroundBlack()->draw('banner-infobiotech-colored');
+$climate->backgroundBlack()->lightGray()->border();
+$climate->backgroundBlack()->white()->out('');
+$climate->backgroundBlack()->white()->bold()->out('infobiotech/php-json-cache client example');
+$climate->backgroundBlack()->white()->out('');
+$climate->backgroundBlack()->lightGray()->border();
 /*
  *
  */
-$climate->out('');
-$climate->inline('Setting a value... ');
-fakeProcessTime();
-$value      = uniqid();
+$climate->backgroundBlack()->white()->out('');
+$climate->backgroundBlack()->white()->inline('Creating Flysystem\Adapter\Local instance... ');
+$filesystemAdapter = new League\Flysystem\Adapter\Local('.');
+usleep(mt_rand(150000, 800000));
+$climate->backgroundBlack()->lightGreen()->bold()->inline('done');
+$climate->backgroundBlack()->white()->out('');
+/*
+ *
+ */
+$climate->backgroundBlack()->white()->out('');
+$climate->backgroundBlack()->white()->inline('Creating JsonCache instance... ');
+$jsonCache         = new Infobiotech\JsonCache($filesystemAdapter, uniqid());
+usleep(mt_rand(150000, 800000));
+$climate->backgroundBlack()->lightGreen()->bold()->inline('done');
+$climate->backgroundBlack()->white()->out('');
+/*
+ *
+ */
+$climate->backgroundBlack()->white()->out('');
+$climate->backgroundBlack()->white()->inline('Generating a random value... ');
+$value             = uniqid();
+usleep(mt_rand(150000, 800000));
+$climate->backgroundBlack()->magenta()->bold()->inline(var_export($value, true));
+/*
+ *
+ */
+$climate->backgroundBlack()->white()->out('');
+$climate->backgroundBlack()->white()->inline('Storing the value... ');
 $jsonCache->set('key', $value);
-$climate->inline($value);
-fakeProcessTime();
+usleep(mt_rand(150000, 800000));
+$climate->backgroundBlack()->lightGreen()->bold()->inline('done');
 /*
  *
  */
-$climate->out('');
-$climate->inline('Getting the value... ');
-fakeProcessTime();
-$checkValue = $jsonCache->get('key');
-$climate->inline($checkValue);
+$climate->backgroundBlack()->white()->out('');
+$climate->backgroundBlack()->white()->inline('Getting the value... ');
+$checkValue        = $jsonCache->get('key');
+usleep(mt_rand(150000, 800000));
+$climate->backgroundBlack()->magenta()->bold()->inline(var_export($checkValue, true));
 /*
  *
  */
-$climate->out('');
-$climate->inline('Verifying the value... ');
-fakeProcessTime();
+$climate->backgroundBlack()->white()->out('');
+$climate->backgroundBlack()->white()->inline('Verifying the value... ');
+usleep(mt_rand(150000, 800000));
 if ($checkValue === $value) {
-    $climate->inline('VERIFIED');
+    $climate->backgroundBlack()->lightGreen()->bold()->inline('verified');
 } else {
-    $climate->red()->bold()->inline('FAILED');
+    $climate->backgroundBlack()->red()->bold()->inline('FAILED');
 }
-fakeProcessTime();
+unset($checkValue, $value);
+usleep(mt_rand(150000, 800000));
 /*
  *
  */
-$climate->out('');
-$climate->out('Cleaning up JsonCache...');
-$climate->out('');
+$climate->backgroundBlack()->white()->out('');
+/*
+ *
+ */
+$climate->backgroundBlack()->white()->out('');
+$climate->backgroundBlack()->white()->inline('Generating a random value... ');
+$ttlValue      = uniqid();
+usleep(mt_rand(150000, 800000));
+$climate->backgroundBlack()->magenta()->bold()->inline(var_export($ttlValue, true));
+/*
+ *
+ */
+$climate->backgroundBlack()->white()->out('');
+$climate->backgroundBlack()->white()->inline('Generating a random TTL... ');
+$ttl           = mt_rand(3, 7);
+usleep(mt_rand(150000, 800000));
+$climate->backgroundBlack()->magenta()->bold()->inline(var_export($ttl, true));
+/*
+ *
+ */
+$climate->backgroundBlack()->white()->out('');
+$climate->backgroundBlack()->white()->inline('Setting the value with the TTL... ');
+$jsonCache->set('ttlkey', $ttlValue, $ttl);
+usleep(mt_rand(150000, 800000));
+$climate->backgroundBlack()->magenta()->bold()->inline(var_export($ttlValue, true));
+/*
+ *
+ */
+$climate->backgroundBlack()->white()->out('');
+$climate->backgroundBlack()->white()->inline('Getting the value before expiration... ');
+$checkTtlValue = $jsonCache->get('ttlkey');
+usleep(mt_rand(150000, 800000));
+$climate->backgroundBlack()->magenta()->bold()->inline(var_export($checkTtlValue, true));
+/*
+ *
+ */
+$climate->backgroundBlack()->white()->out('');
+$climate->backgroundBlack()->white()->inline('Verifying the value before expiration... ');
+if ($checkTtlValue === $ttlValue) {
+    usleep(mt_rand(150000, 800000));
+    $climate->backgroundBlack()->lightGreen()->bold()->inline('verified');
+} else {
+    usleep(mt_rand(150000, 800000));
+    $climate->backgroundBlack()->red()->bold()->inline('FAILED');
+}
+unset($checkTtlValue, $ttlValue);
+/*
+ *
+ */
+$climate->backgroundBlack()->white()->out('');
+$climate->backgroundBlack()->white()->out('Wait for value expiration');
+$progress = $climate->backgroundBlack()->white()->progress()->total(100);
+for ($i = 0; $i <= 100; $i++) {
+    $progress->current($i);
+    usleep(mt_rand($ttl * 10000, $ttl * 20000));
+}
+/*
+ *
+ */
+$climate->backgroundBlack()->white()->inline('Getting the value after expiration... ');
+usleep(mt_rand(150000, 800000));
+$checkExpiredValue = $jsonCache->get('ttlkey');
+$climate->backgroundBlack()->magenta()->bold()->inline(var_export($checkExpiredValue, true));
+/*
+ *
+ */
+$climate->backgroundBlack()->white()->out('');
+$climate->backgroundBlack()->white()->inline('Verifying empty value after expiration... ');
+usleep(mt_rand(150000, 800000));
+if (empty($checkExpiredValue)) {
+    $climate->backgroundBlack()->lightGreen()->bold()->inline('verified');
+} else {
+    $climate->backgroundBlack()->red()->bold()->inline('FAILED: value is '.var_export($checkExpiredValue, true));
+}
+unset($checkTtlValue, $ttlValue);
+usleep(mt_rand(150000, 800000));
+/*
+ *
+ */
+$climate->backgroundBlack()->white()->out('');
+/*
+ *
+ */
+$climate->backgroundBlack()->white()->out('');
+$climate->backgroundBlack()->white()->inline('Cleaning up JsonCache...');
 $jsonCache->clear();
-fakeProcessTime();
+usleep(mt_rand(150000, 800000));
+$climate->backgroundBlack()->lightGreen()->bold()->inline('done');
+/*
+ *
+ */
+$climate->backgroundBlack()->white()->out('');
+/*
+ *
+ */
+$climate->backgroundBlack()->white()->out('');
+$climate->backgroundBlack()->white()->inline('Unsetting local variable...');
+unset($jsonCache);
+usleep(mt_rand(150000, 800000));
+$climate->backgroundBlack()->lightGreen()->bold()->inline('done');
+$climate->backgroundBlack()->white()->out('');
+usleep(mt_rand(150000, 800000));
+$climate->backgroundBlack()->white()->out('');
+/*
+ *
+ */
+unset($climate);
+/*
+ *
+ */
+exit(1);
