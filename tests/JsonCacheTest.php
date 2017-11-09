e24d5bd4b240e5f8ce4f0212329daa8a867455c3
Alessandro Raffa <contact@alessandroraffa.eu>
2017-11-09 01:33:07 +0100

    PSR-16 InvalidArgumentException tests.

--- tests/JsonCacheTest.php
+++ tests/JsonCacheTest.php
@@ -52,38 +52,238 @@ class JsonCacheTest extends \PHPUnit\Framework\TestCase
         $this->assertTrue($cache->clear());
     }
 
+    public function testInvalidArgumentException1()
+    {
+        /*
+         * Create instance
+         */
+        $cache = new Driver(
+                new \League\Flysystem\Adapter\Local(dirname(__FILE__)), static::TEST_NAMESPACE_2
+        );
+        /*
+         * Expecting an InvalidArgumentException
+         */
+        $this->expectException('InvalidArgumentException');
+        $cache->set(null, static::TEST_STRING_VALUE);
+    }
+
+    public function testInvalidArgumentException2()
+    {
+        /*
+         * Create instance
+         */
+        $cache = new Driver(
+                new \League\Flysystem\Adapter\Local(dirname(__FILE__)), static::TEST_NAMESPACE_1
+        );
+        /*
+         * Expecting an InvalidArgumentException
+         */
+        $this->expectException('InvalidArgumentException');
+        $cache->get(null);
+    }
+
+    public function testInvalidArgumentException3()
+    {
+        /*
+         * Create instance
+         */
+        $cache = new Driver(
+                new \League\Flysystem\Adapter\Local(dirname(__FILE__)), static::TEST_NAMESPACE_2
+        );
+        /*
+         * Expecting an InvalidArgumentException
+         */
+        $this->expectException('InvalidArgumentException');
+        $cache->delete(null);
+    }
+
+    public function testInvalidArgumentException4()
+    {
+        /*
+         * Create instance
+         */
+        $cache = new Driver(
+                new \League\Flysystem\Adapter\Local(dirname(__FILE__)), static::TEST_NAMESPACE_1
+        );
+        /*
+         * Expecting an InvalidArgumentException
+         */
+        $this->expectException('InvalidArgumentException');
+        $cache->getMultiple(null);
+    }
+
+    public function testInvalidArgumentException5()
+    {
+        /*
+         * Create instance
+         */
+        $cache = new Driver(
+                new \League\Flysystem\Adapter\Local(dirname(__FILE__)), static::TEST_NAMESPACE_2
+        );
+        /*
+         * Expecting an InvalidArgumentException
+         */
+        $this->expectException('InvalidArgumentException');
+        $cache->setMultiple(null);
+    }
+
+    public function testInvalidArgumentException6()
+    {
+        /*
+         * Create instance
+         */
+        $cache = new Driver(
+                new \League\Flysystem\Adapter\Local(dirname(__FILE__)), static::TEST_NAMESPACE_1
+        );
+        /*
+         * Expecting an InvalidArgumentException
+         */
+        $this->expectException('InvalidArgumentException');
+        $cache->deleteMultiple(null);
+    }
+
+    public function testInvalidArgumentException7()
+    {
+        /*
+         * Create instance
+         */
+        $cache = new Driver(
+                new \League\Flysystem\Adapter\Local(dirname(__FILE__)), static::TEST_NAMESPACE_2
+        );
+        /*
+         * Expecting an InvalidArgumentException
+         */
+        $this->expectException('InvalidArgumentException');
+        $cache->has(null);
+    }
+
     public function testAll()
     {
         /*
          * Create instance
          */
         $cache = new Driver(
+                new \League\Flysystem\Adapter\Local(dirname(__FILE__)), self::TEST_NAMESPACE_1
+        );
+        /*
+         *
+         */
+        $this->assertTrue($cache->set('key', static::TEST_STRING_VALUE));
+        /*
+         *
+         */
+        $this->assertEquals(static::TEST_STRING_VALUE, $cache->get('key'));
+        /*
+         *
+         */
+        $this->assertNull($cache->get('missing'));
+        /*
+         *
+         */
+        $this->assertTrue($cache->clear());
+        /*
+         *
+         */
+        $this->assertNull($cache->get('key'));
+        /*
+         *
+         */
+        $this->assertTrue($cache->set('key', static::TEST_STRING_VALUE, 1));
+        /*
+         *
+         */
+        $this->assertEquals(static::TEST_STRING_VALUE, $cache->get('key'));
+        /*
+         *
+         */
+        $this->assertEquals(0, sleep(2));
+        /*
+         *
+         */
+        $this->assertNull($cache->get('key'));
+        /*
+         *
+         */
+        $this->assertTrue($cache->set('int', static::TEST_INT_VALUE));
+        /*
+         *
+         */
+        $this->assertEquals(static::TEST_INT_VALUE, $cache->get('int'));
+        /*
+         *
+         */
+        $this->assertNull($cache->get('key'));
+        /*
+         *
+         */
+        $this->assertTrue($cache->clear());
+        /*
+         *
+         */
+        $this->assertNull($cache->get('int'));
+        /*
+         *
+         */
+        $this->assertTrue($cache->set('int', static::TEST_INT_VALUE, 1));
+        /*
+         *
+         */
+        $this->assertEquals(static::TEST_INT_VALUE, $cache->get('int'));
+        /*
+         *
+         */
+        $this->assertTrue($cache->delete('int'));
+        /*
+         *
+         */
+        $this->assertNull($cache->get('int'));
+        /*
+         *
+         */
+        $this->assertTrue($cache->clear());
+        /*
+         *
+         */
+        $this->assertTrue($cache->setMultiple($this->multipleValues));
+        /*
+         *
+         */
+        $this->assertTrue($cache->has('key1'));
+        /*
+         *
+         */
+        $this->assertEquals($this->multipleValues, $cache->getMultiple(array_keys($this->multipleValues)));
+        /*
+         *
+         */
+        $this->assertTrue($cache->deleteMultiple(array_keys($this->multipleValues)));
+        /*
+         *
+         */
+        $this->assertTrue($cache->clear());
+    }
+
+    public function testClear()
+    {
+        /*
+         * Create instance
+         */
+        $cache1 = new Driver(
+                new \League\Flysystem\Adapter\Local(dirname(__FILE__)), self::TEST_NAMESPACE_1
+        );
+        /*
+         *
+         */
+        $this->assertTrue($cache1->clear());
+        /*
+         * Create instance
+         */
+        $cache2 = new Driver(
                 new \League\Flysystem\Adapter\Local(dirname(__FILE__)), self::TEST_NAMESPACE_2
         );
-        //$this->expectException($cache->set(null, static::TEST_STRING_VALUE));
-        $this->assertTrue($cache->set('key', static::TEST_STRING_VALUE));
-        $this->assertEquals(static::TEST_STRING_VALUE, $cache->get('key'));
-        $this->assertNull($cache->get('missing'));
-        $this->assertTrue($cache->clear());
-        $this->assertNull($cache->get('key'));
-        $this->assertTrue($cache->set('key', static::TEST_STRING_VALUE, 1));
-        $this->assertEquals(static::TEST_STRING_VALUE, $cache->get('key'));
-        $this->assertEquals(0, sleep(2));
-        $this->assertNull($cache->get('key'));
-        $this->assertTrue($cache->set('int', static::TEST_INT_VALUE));
-        $this->assertEquals(static::TEST_INT_VALUE, $cache->get('int'));
-        $this->assertNull($cache->get('key'));
-        $this->assertTrue($cache->clear());
-        $this->assertNull($cache->get('int'));
-        $this->assertTrue($cache->set('int', static::TEST_INT_VALUE, 1));
-        $this->assertEquals(static::TEST_INT_VALUE, $cache->get('int'));
-        $this->assertTrue($cache->delete('int'));
-        $this->assertNull($cache->get('int'));
-        $this->assertTrue($cache->clear());
-        $this->assertTrue($cache->setMultiple($this->multipleValues));
-        $this->assertTrue($cache->has('key1'));
-        $this->assertEquals($this->multipleValues, $cache->getMultiple(array_keys($this->multipleValues)));
-        $this->assertTrue($cache->deleteMultiple(array_keys($this->multipleValues)));
-        $this->assertTrue($cache->clear());
+        /*
+         *
+         */
+        $this->assertTrue($cache2->clear());
     }
 }
